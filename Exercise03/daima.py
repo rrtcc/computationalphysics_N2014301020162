35fb04a252765f7db62f4a197665c225d5c00024
rrtcc <1176206762@qq.com>
2016-09-25 19:47:15 +0800

    Add files via upload

---
+++ Exercise03/daima.py
@@ -0,0 +1,16 @@
+import time 
+import os
+os.system('cls')
+time.sleep(0)
+os.system('cls')
+for i in range(0,100):
+    print(i*' '+'   ##    #          ##    #      #    #####    #      #    ##    #      #')
+    print(i*' '+'  #  #   #         #  #   ##     #    #    #   #      #   #  #   ##     #')
+    print(i*' '+' #    #  #        #    #  # #    #    #     #  #      #  #    #  # #    #')
+    print(i*' '+'######## #       ######## #  ##  #    #      # #      # ######## #  #   #')
+    print(i*' '+'#      # #       #      # #   #  #    #      # #      # #      # #   #  #')
+    print(i*' '+'#      # #       #      # #    # #    #     #  #      # #      # #    # #')
+    print(i*' '+'#      # #       #      # #     ##    #    #   #      # #      # #     ##')
+    print(i*' '+'#      # ####### #      # #      #    #####     ######  #      # #      #')
+    time.sleep(0.2)
+    os.system('cls')
