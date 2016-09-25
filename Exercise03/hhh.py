b857f098131cbcc0676b6d7fc4090dd22583d702
rrtcc <1176206762@qq.com>
2016-09-26 00:21:38 +0800

    Add files via upload

---
+++ Exercise03/hhh.py
@@ -0,0 +1,142 @@
+import time
+import os
+i = os.system('cls')
+time.sleep(0.5)
+print'      ###'
+print'      ###'
+print'       #'
+print'    ## # ##'
+print'      ###'
+print'      # #'
+print'     #   #'
+print'    #     #'
+time.sleep(0.3)
+i = os.system('cls')
+print'     #    ###'
+print' ##   # ##'
+print' #######'
+print' ##   # ##'
+print'     #    ###'
+time.sleep(0.3)
+i = os.system('cls')
+print'    #     #'
+print'     #   #'
+print'      # #'
+print'       #'
+print'      ### '
+print'    ## # ##'
+print'       # '
+print'      ###'
+print'      ###'
+time.sleep(0.3)
+i = os.system('cls')
+print' ###    #'
+print'    ## #   ##'
+print'      #######'
+print'    ## #   ##'
+print' ###    #'
+time.sleep(0.3)
+i = os.system('cls')
+print'      ###'
+print'      ###'
+print'       #'
+print'    ## # ##'
+print'      ###'
+print'      # #'
+print'     #   #'
+print'    #     #'
+time.sleep(0.3)
+i = os.system('cls')
+print'    #    ###'
+print' ##   # ##'
+print' #######'
+print' ##   # ##'
+print'    #    ###'
+time.sleep(0.3)
+i = os.system('cls')
+print'    #     #'
+print'     #   #'
+print'      # #'
+print'       #'
+print'      ### '
+print'    ## # ##'
+print'       # '
+print'      ###'
+print'      ###'
+time.sleep(0.3)
+i = os.system('cls')
+print' ###    #'
+print'    ## #   ##'
+print'     #######'
+print'    ## #   ##'
+print' ###    #'
+time.sleep(0.3)
+i = os.system('cls')
+print'      ###'
+print'      ###'
+print'       #'
+print'    ## # ##'
+print'      ###'
+print'      # #'
+print'     #   #'
+print'    #     #'
+time.sleep(0.3)
+i = os.system('cls')
+print'     #    ###'
+print' ##   # ##'
+print' #######'
+print' ##   # ##'
+print'     #    ###'
+time.sleep(0.3)
+i = os.system('cls')
+print'    #     #'
+print'     #   #'
+print'      # #'
+print'       #'
+print'      ### '
+print'    ## # ##'
+print'       # '
+print'      ###'
+print'      ###'
+time.sleep(0.3)
+i = os.system('cls')
+print' ###    #'
+print'    ## #   ##'
+print'      #######'
+print'    ## #   ##'
+print' ###    #'
+time.sleep(0.3)
+i = os.system('cls')
+print'      ###'
+print'      ###'
+print'       #'
+print'    ## # ##'
+print'      ###'
+print'      # #'
+print'     #   #'
+print'    #     #'
+time.sleep(0.3)
+i = os.system('cls')
+print'    #    ###'
+print' ##   # ##'
+print' #######'
+print' ##   # ##'
+print'    #    ###'
+time.sleep(0.3)
+i = os.system('cls')
+print'    #     #'
+print'     #   #'
+print'      # #'
+print'       #'
+print'      ### '
+print'    ## # ##'
+print'       # '
+print'      ###'
+print'      ###'
+time.sleep(0.3)
+i = os.system('cls')
+print' ###    #'
+print'    ## #   ##'
+print'     #######'
+print'    ## #   ##'
+print' ###    #'
